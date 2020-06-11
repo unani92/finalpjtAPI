02475957a44dc766ea3a2c7269979844d71aa0aa
unani <unani92@naver.com>
2020-06-11 17:06:20 +0900

    loaddata success

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    age = models.IntegerField()
+    age = models.IntegerField(null=True)
