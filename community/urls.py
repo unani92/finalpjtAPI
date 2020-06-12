33ad6400ab1af53cd0f73de98669905241efdca9
unani <unani92@naver.com>
2020-06-12 18:23:52 +0900

    change function bv to class bv

--- community/urls.py
+++ community/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('movies/', views.index), # 모든 영화 조회하기
-    path('movies/home/', views.movie_best3),
-    path('movies/<int:movie_pk>/', views.moviedetail), # 개별 영화 조회하기
-    path('articles/<int:pk>/', views.article_view_create), # 개별 아티클 조회하기 및 생성하기
-    path('articles/<int:article_pk>/comments/', views.comment_create) # 댓글 작성하기
+    path('movies/', views.MovieList.as_view()), # 모든 영화 조회하기
+    path('movies/home/', views.MovieBest3.as_view()),
+    path('movies/<int:movie_pk>/', views.MovieDetail.as_view()), # 개별 영화 조회하기
+    path('articles/<int:pk>/', views.ArticleList.as_view()), # 개별 아티클 조회하기 및 생성하기
+    path('articles/<int:article_pk>/comments/', views.CommentList.as_view()) # 댓글 작성하기
 ]
